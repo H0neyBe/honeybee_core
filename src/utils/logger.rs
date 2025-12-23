cec99f3871c5db05db4f4b08ef673e0d4e7f0a5e
HqNw <redalabeb166@gmail.com>
2025-12-23 18:45:20 +0200

    added more verbose logging and option to force colors to work in tools like bacon

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,10 +1,15 @@
 use std::fs::OpenOptions;
+use std::thread;
 
 use bee_config::Config;
 use colored::Colorize;
 
 pub fn init_logger(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
   let log_level: log::LevelFilter = config.logging.level.parse().unwrap();
+
+  if config.logging.force_color {
+    colored::control::set_override(true);
+  }
 
   // Console output with colors
   let console_dispatch = fern::Dispatch::new()
@@ -17,14 +22,51 @@ pub fn init_logger(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
         log::Level::Trace => record.level().to_string().purple().bold(),
       };
 
+      // Get thread name or ID
+      let mut thread_info = thread::current()
+        .name()
+        .map(|n| n.to_string())
+        .unwrap_or_else(|| format!("thread-{:?}", thread::current().id()));
+
+      // Try to get Tokio task name if available
+      if let Ok(handle) = tokio::runtime::Handle::try_current() {
+        if let Some(task_id) = tokio::task::try_id() {
+          thread_info = format!("{}[task-{:?}]", thread_info, task_id);
+        }
+      }
+
+      // Get target (module path) and line number
+      #[cfg(debug_assertions)]
+      let location = if let Some(line) = record.line() {
+        format!("{}:{}", record.target(), line)
+      } else {
+        record.target().to_string()
+      };
+
+      #[cfg(debug_assertions)]
       out.finish(format_args!(
-        "[{}][{}] {}",
+        "[{}][{}][{}][{}] {}",
         chrono::Local::now()
-          .format("%Y-%m-%d %H:%M:%S.%9f")
+          .format("%Y-%m-%d %H:%M:%S.%3f")
           .to_string()
           .black()
           .dimmed(),
         level_colored,
+        thread_info.cyan(),
+        location.bright_black(),
+        message
+      ));
+
+      #[cfg(not(debug_assertions))]
+      out.finish(format_args!(
+        "[{}][{}][{}] {}",
+        chrono::Local::now()
+          .format("%Y-%m-%d %H:%M:%S.%3f")
+          .to_string()
+          .black()
+          .dimmed(),
+        level_colored,
+        thread_info.cyan(),
         message
       ))
     })
@@ -39,10 +81,32 @@ pub fn init_logger(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
 
     let file_dispatch = fern::Dispatch::new()
       .format(|out, message, record| {
+        // Get thread name or ID
+        let mut thread_info = thread::current()
+          .name()
+          .map(|n| n.to_string())
+          .unwrap_or_else(|| format!("thread-{:?}", thread::current().id()));
+
+        // Try to get Tokio task name if available
+        if let Ok(handle) = tokio::runtime::Handle::try_current() {
+          if let Some(task_id) = tokio::task::try_id() {
+            thread_info = format!("{}[task-{:?}]", thread_info, task_id);
+          }
+        }
+
+        // Get target (module path) and line number
+        let location = if let Some(line) = record.line() {
+          format!("{}:{}", record.target(), line)
+        } else {
+          record.target().to_string()
+        };
+
         out.finish(format_args!(
-          "[{}][{}] {}",
-          chrono::Local::now().format("%Y-%m-%d %H:%M:%S.%9f"),
+          "[{}][{}][{}][{}] {}",
+          chrono::Local::now().format("%Y-%m-%d %H:%M:%S.%3f"),
           record.level(),
+          thread_info,
+          location,
           message
         ))
       })
@@ -52,7 +116,6 @@ pub fn init_logger(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
           .create(true)
           .truncate(true)
           .write(true)
-          // .append(true)
           .open(file)?,
       );
 
@@ -60,5 +123,6 @@ pub fn init_logger(config: &Config) -> Result<(), Box<dyn std::error::Error>> {
   }
 
   base_dispatch.apply()?;
+
   Ok(())
 }
